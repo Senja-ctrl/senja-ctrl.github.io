46c52546cc8a968ae12330a95f4ea8b74351715e
Senja-ctrl <simpson.julian@outlook.com>
2026-04-01 09:18:20 +0200

    dafsdfa

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,14 +5,14 @@ function SetWindowWidthOnLoad() {
 
 function SetWindowWidth() {
     const element = document.getElementById("i02")
-    const x = 50;
-    const y1 = 55;
-    const y2 = y1 + 5;
+    const x = 55;
+    const y1 = 40;
+    const y2 = y1 + 4;
     const y3 = y2 + 1;
-    const y4 = y3 + 5;
-    const w = window.innerWidth - x;
+    const y4 = y3 + 4;
+    const w = window.innerWidth - 40;
     const w2 = w - 5;
-    const array = [[x, y1], [w2, y1], [w, y1 - 15], [w, y2], [x + 10, y2], [x + 10, y3], [w2 - 10, y3], [w - 10, y3 - 15], [w - 10, y4], [x, y4], [x, y1]]
+    const array = [[x, y1], [w2, y1], [w, y1 - 15], [w, y2], [x + 10, y3], [x + 10, y3], [w2 - 5, y3], [w - 5, y3 - 15], [w - 5, y4], [x, y4], [x, y1]]
     element.setAttribute("points", array);
 }
 
